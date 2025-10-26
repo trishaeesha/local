4bcfbe35dc3f5c867366823b37f79ccd24d60300
trishaeesha <triishaa361@gmail.com>
2025-10-26 17:08:39 +0530

    add intial files

---
+++ index.h
@@ -0,0 +1,9 @@
+rintf("Hello, World!\n");           
++#endif // GIT_HUP_INDEX_H
+#ifndef GIT_HUP_INDEX_H
+#define GIT_HUP_INDEX_H 
+// Your code goes here
+printf("Hello, World!\n");
+#endif // GIT_HUP_INDEX_H
+
+#endif // GIT_HUP_INDEX_H
